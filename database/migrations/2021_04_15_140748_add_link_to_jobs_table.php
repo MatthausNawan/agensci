e71050ba14b109024157c5c0be54130c382f066a
Matthaus Nawan <matthausnawan@gmail.com>
2021-04-15 14:36:27 -0300

    fix migration class

--- database/migrations/2021_04_15_140748_add_link_to_jobs_table.php
+++ database/migrations/2021_04_15_140748_add_link_to_jobs_table.php
@@ -26,7 +26,7 @@ class AddLinkToJobsTable extends Migration
     public function down()
     {
         Schema::table('jobs', function (Blueprint $table) {
-            $table->dropColumn('link')
+            $table->dropColumn('link');
         });
     }
 }
