713dd1eef45af0db9365548c462dabec0575b57b
Matthaus Nawan <matthausnawan@gmail.com>
2021-04-24 00:51:33 -0300

    fix ui slide card

--- resources/views/frontend/pages/home/_partials/card-slider.blade.php
+++ resources/views/frontend/pages/home/_partials/card-slider.blade.php
@@ -1,5 +1,5 @@
-<div class="links-wrapper py-2">
-    <ul class="nav nav-tabs px-4" id="myTab" role="tablist">
+<div class="links-wrapper">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
         @foreach($se as $seg)
         <li class="nav-item">
             <a class="nav-link bg-light text-white {{ $loop->first ? 'active' : 'ml-1' }}" id="{{$seg->id}}-tab" data-toggle="tab" href="#tab{{$seg->id}}" role="tab" aria-controls="{{$seg->id}}" aria-selected="{{ $loop->first ? 'true' : 'false' }}">{{$seg->name}}</a>
@@ -8,16 +8,20 @@
     </ul>
     <div class="tab-content" id="myTabContent">
         @foreach($events as $key => $event)
-        <div class="tab-pane px-4 fade {{ $loop->first ? 'show active' : '' }}" id="tab{{$key}}" role="tabpanel" aria-labelledby="{{$key}}-tab">
-            <div class="card-group {{ $event->count() >= 4 ? $class : ' '  }}">
+        <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="tab{{$key}}" role="tabpanel" aria-labelledby="{{$key}}-tab">
+            <!-- <div class="card-group {{ $event->count() >= 4 ? $class : ' '  }}"> -->
+            <div class="card-group {{$class}}">
                 @foreach($event as $e)
                 <div class="card">
-                    <img class="card-img-top" src="{{$e->banner ? $e->banner->getUrl() : ''}}" alt="Card image cap">
+                    <img class="card-img-top" src="{{$e->banner ? $e->banner->getUrl() : ''}}" alt="Card image cap" style="height:120px;width:100%">
                     <div class="card-body">
                         <p class="card-title small text-bold card-text">{{ $e->title }}</p>
                         <div class="details d-flex flex-column">
                             <span class="details-text">Data Evento: {{$e->start_date }}</span>
                             <span class="details-text">Local: {{ $e->location ?? 'Local do Evento' }}</span>
+                            @if($e->link)
+                                <span class="details-text">Link:<a href="{{ $e->link ?? ''}}" target="_blank"> {{ $e->link ?? ''}}</a></span>
+                            @endif
                         </div>
                     </div>
                 </div>
